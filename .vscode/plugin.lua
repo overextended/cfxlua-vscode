f796d2dd46a0c34eff3836acefb6a5f66a05fc3e
Linden <65407488+thelindat@users.noreply.github.com>
2021-11-14 06:58:08 +1100

    define vectors, matrix, and quat types

--- .vscode/plugin.lua
+++ .vscode/plugin.lua
@@ -1,6 +1,18 @@
 -- if this file exists, then change setting `Lua.runtime.plugin`
 -- see https://github.com/sumneko/lua-language-server/wiki/Plugin
 
+---@class vector
+---@class vector2
+---@class vector3
+---@class vector4
+---@class mat
+---@class quat
+
+---@alias vec vector
+---@alias vec2 vector2
+---@alias vec3 vector3
+---@alias vec4 vector4
+
 function OnSetText(uri, text)
     local diffs = {}
 
