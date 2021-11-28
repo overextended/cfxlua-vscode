ca67f68d394d8bb9c7fad7fd88ab8c3e47959f6b
Linden <65407488+thelindat@users.noreply.github.com>
2021-11-29 07:12:28 +1100

    feat(json): define _most_ functions
    Some of this stuff shouldn't really be used for compatibility.

--- library/json.lua
+++ library/json.lua
@@ -3,13 +3,38 @@ json = {}
 
 -- TODO: annotations
 
+---@return table
+--- Returns a metatable with the `array` __jsontype.
 function json.array() end
-function json.decode() end
-function json.encode() end
-function json.getoption() end
-function json.isarray() end
-function json.isobject() end
+
+---@param string string
+--- Refer to [documentation](https://github.com/gottfriedleibniz/lua-rapidjson#decoding) for additional options.
+function json.decode(string, ...) end
+
+---@param string string
+---@param options? table {indent=true}
+--- Refer to [documentation](https://github.com/gottfriedleibniz/lua-rapidjson#encoding) for additional options.
+function json.encode(string, options) end
+
+function json.getoption(...) end
+
+---@param value table
+---@return boolean
+--- Return true if the provided table has the 'array' __jsontype.
+function json.isarray(value) end
+
+---@param value table
+---@return boolean
+--- Return true if the provided table has the 'object' __jsontype.
+function json.isobject(value) end
+
 function json.null() end
+
+---@return table
+--- Returns a metatable with the `object` __jsontype.
 function json.object() end
-function json.setoption() end
-function json.use_lpeg() end
+
+function json.setoption(...) end
+
+---@param bool boolean
+function json.use_lpeg(bool) end
