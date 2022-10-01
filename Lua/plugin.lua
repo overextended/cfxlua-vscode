d4be3880be65c80135ed774791ffabbb3ed17db7
Linden <65407488+thelindat@users.noreply.github.com>
2022-10-02 04:47:42 +1000

    fix(Lua/plugin): ignore pairs and ipairs for in unpacking
    Should be the only cases that use in afaik.

--- Lua/plugin.lua
+++ Lua/plugin.lua
@@ -16,21 +16,26 @@ function OnSetText(uri, text)
 
 	-- prevent diagnostic errors from in unpacking (a, b, c in t)
 	for vars, inPos, afterInPos, tablePos, tableName, finishPos in text:gmatch '([_%w, ]*)%s+()in()%s+()([_%w]*)()' do
-		if vars:sub(1, 3) ~= 'for' then
+		if tableName ~= 'ipairs' and tableName ~= 'pairs' and vars:sub(1, 3) ~= 'for' then
 			-- replace 'in' with '='
-			diffs[#diffs + 1] = {
+			count = count + 1
+			diffs[count] = {
 				start = inPos,
 				finish = afterInPos - 1,
 				text = '='
 			}
+
 			-- replace 't' with 't.a, t.b, t.c'
 			if vars:sub(1, 5) == 'local' then vars = vars:sub(7) end
 			local tableVars = ''
+
 			for varName in vars:gsub('%s+', ''):gmatch('([_%w]+)') do
 				if #tableVars > 0 then tableVars = tableVars .. ',' end
 				tableVars = tableVars .. tableName .. '.' .. varName
 			end
-			diffs[#diffs + 1] = {
+
+			count = count + 1
+			diffs[count] = {
 				start = tablePos,
 				finish = finishPos - 1,
 				text = tableVars
