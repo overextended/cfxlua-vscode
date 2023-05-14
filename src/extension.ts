3a29c2e5b8080f4174dd9944dc2dfba7af300095
Linden <65407488+thelindat@users.noreply.github.com>
2023-05-14 15:58:15 +1000

    fix(extension): await config.update
    This used to work; behaviour change?
    Resolves #22.

--- src/extension.ts
+++ src/extension.ts
@@ -4,7 +4,7 @@ import { join as pathJoin } from "path";
 const extensionId = "overextended.cfxlua-vscode";
 
 function getExtensionPath(extensionId: string) {
-  return vscode.extensions.getExtension(extensionId)?.extensionPath;
+  return vscode.extensions.getExtension(extensionId)!.extensionPath;
 }
 
 function setPlugin(enable: boolean) {
@@ -28,58 +28,59 @@ function setPlugin(enable: boolean) {
   }
 }
 
-function setExternalLibrary(folder: string, enable: boolean) {
+async function setExternalLibrary(folder: string, enable: boolean) {
   const extensionPath = getExtensionPath(extensionId);
   const folderPath = pathJoin(extensionPath!, "Lua", folder);
   const config = vscode.workspace.getConfiguration("Lua");
-  const library: string[] | undefined = config.get("workspace.library");
+  const library: string[] = config.get("workspace.library")!;
 
-  if (library && extensionPath) {
-    // remove any older versions of our path e.g. "publisher.name-0.0.1"
-    for (let i = library.length - 1; i >= 0; i--) {
-      const el = library[i];
-      const isSelfExtension = el.indexOf(extensionId) > -1;
-      const isCurrentVersion = el.indexOf(extensionPath) > -1;
-      if (isSelfExtension && !isCurrentVersion) {
-        library.splice(i, 1);
-      }
+  for (let i = library.length - 1; i >= 0; i--) {
+    const el = library[i];
+    const isSelfExtension = el.includes(extensionId);
+    const isCurrentVersion = el.includes(extensionPath);
+    if (isSelfExtension && !isCurrentVersion) {
+      library.splice(i, 1);
     }
-
-    const index = library.indexOf(folderPath);
-
-    if (enable) {
-      if (index === -1) {
-        library.push(folderPath);
-      }
-    } else {
-      if (index > -1) {
-        library.splice(index, 1);
-      }
-    }
-
-    config.update("workspace.library", library, true);
   }
+
+  const index = library.indexOf(folderPath);
+
+  if (enable) {
+    if (index === -1) {
+      library.push(folderPath);
+    }
+  } else {
+    if (index > -1) {
+      library.splice(index, 1);
+    }
+  }
+
+  await config.update("workspace.library", library, true);
 }
 
 // todo: command or such to easily swap out the native library
-export function setNativeLibrary() {
+async function setNativeLibrary() {
   const config = vscode.workspace.getConfiguration("cfxlua");
   let game: string = config.get("game") || "GTAV";
   game = game ? game.toUpperCase() : "GTAV";
 
-  if (game !== "GTAV") { setExternalLibrary(`natives/GTAV`, false); }
-  if (game !== "RDR3") { setExternalLibrary(`natives/RDR3`, false); }
+  if (game !== "GTAV") {
+    await setExternalLibrary(`natives/GTAV`, false);
+  }
+  if (game !== "RDR3") {
+    await setExternalLibrary(`natives/RDR3`, false);
+  }
 
-  setExternalLibrary(`natives/${game}`, true);
+  await setExternalLibrary(`natives/${game}`, true);
 }
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-  setPlugin(true);
-  setExternalLibrary("runtime", true);
-  setExternalLibrary("natives/CFX-NATIVE", true);
-  setNativeLibrary();
+export async function activate(context: vscode.ExtensionContext) {
+  await setPlugin(true);
+  await setExternalLibrary("runtime", true);
+  await setExternalLibrary("natives/CFX-NATIVE", true);
+  await setNativeLibrary();
 }
 
 // this method is called when your extension is deactivated
