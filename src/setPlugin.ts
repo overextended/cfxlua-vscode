40d0a82dc31be15685f853c1feafacea0859d35e
Linden <65407488+thelindat@users.noreply.github.com>
2023-07-03 19:48:22 +1000

    refactor(setPlugin): default Lua configuration
    Add ignored directories.
    Don't overwrite user config for array values.

--- src/setPlugin.ts
+++ src/setPlugin.ts
@@ -9,12 +9,47 @@ export default async function setPlugin(enable: boolean) {
 
   if (enable) {
     await config.update("runtime.version", "Lua 5.4", settingsScope);
+    await config.update("runtime.plugin", pluginPath, settingsScope);
+
+    // Support extra symbols in LuaParser
+    const nonstandardSymbol: string[] =
+      config.get("runtime.nonstandardSymbol") || [];
+    [
+      "/**/",
+      "`",
+      "+=",
+      "-=",
+      "*=",
+      "/=",
+      "<<=",
+      ">>=",
+      "&=",
+      "|=",
+      "^=",
+    ].forEach((item) => {
+      if (!nonstandardSymbol.includes(item)) {
+        nonstandardSymbol.push(item);
+      }
+    });
+
     await config.update(
       "runtime.nonstandardSymbol",
-      ["/**/", "`", "+=", "-=", "*=", "/=", "<<=", ">>=", "&=", "|=", "^="],
+      nonstandardSymbol,
       settingsScope
     );
-    await config.update("runtime.plugin", pluginPath, settingsScope);
+
+    // Disable diagnostics for files/directories (using .gitignore grammar)
+    // Drastically improves time to load workspace
+    const ignoreDir: string[] = config.get("workspace.ignoreDir") || [];
+
+    [".vscode", ".git", ".github", "node_modules"].forEach((item) => {
+      if (!ignoreDir.includes(item)) {
+        ignoreDir.push(item);
+      }
+    });
+
+    await config.update("workspace.ignoreDir", ignoreDir, settingsScope);
+
     return;
   }
 
