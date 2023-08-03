5b6a192624f0e3aa11b88aba9e59f63b67a86666
Linden <65407488+thelindat@users.noreply.github.com>
2023-08-03 18:59:19 +1000

    feat(plugin): rewrite in parser
    Doka's happiness is all that matters (I am notokok).
    Unpacking tables, nested tables, function returns should work better.. maybe.
    Maybe it's time to learn and use a proper parser.

--- Lua/test.lua
+++ Lua/test.lua
@@ -30,3 +30,36 @@ print(bar)
 print(bar2)
 
 GetPlayerPed()
+
+local tbl = {
+	a = 'a',
+	b = 'b',
+	c = { d = 'd' },
+	["hands in pockets"] = { e = 'e' },
+	i = { ["j"] = { k = 'k' } }
+}
+
+local please = {
+	god = {
+		---@param a string
+		---@param b any[]
+		---@return { g: string, h: number }
+		help = function(a, b) return { 'g', 1 } end
+	}
+}
+
+---@type string, string
+local a, b in tbl
+---@type string
+local c = "hands in pockets"
+---@type string
+local d in tbl.c
+---@type string
+local e in tbl["hands in pockets"]
+---@type { e: string }
+local f = tbl["hands in pockets"]
+---@type string, number
+local g, h in please.god.help('me', { 'prayge', { ["a"] = 'god save the ~~queen~~ king' } })
+---@type string
+local k in tbl.i["j"]
+
