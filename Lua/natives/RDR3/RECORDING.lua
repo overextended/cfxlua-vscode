b37aebdf208b0f0436554c6d11000cfe86226b4c
Linden <65407488+thelindat@users.noreply.github.com>
2023-12-28 20:11:34 +1100

    refactor: add lua library as a submodule

--- Lua/natives/RDR3/RECORDING.lua
+++
@@ -1,9 +0,0 @@
----@meta
-
----**`RECORDING` `client`**  
----[Native Documentation](https://alloc8or.re/rdr3/nativedb/?n=0xA8C44C13419634F2)  
----nullsub, doesn't do anything
----
----Old name: _STOP_RECORDING_THIS_FRAME
-function ReplayPreventRecordingThisFrame() end
-
--- Lua/natives/RDR3/UIPINNING.lua
+++
@@ -1,9 +0,0 @@
----@meta
-
----**`UIPINNING` `client`**  
----[Native Documentation](https://alloc8or.re/rdr3/nativedb/?n=0x3138582E0A13BFAB)  
----This native does not have an official description.
----@param hash number | string
----@return any
-function UipinningGetTooltipText(hash) end
-
