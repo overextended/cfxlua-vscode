973bb4584db63f28180a7d2db17d018732ba25b5
Linden <65407488+thelindat@users.noreply.github.com>
2025-03-17 19:16:56 +1100

    refactor(extension): only update storagePath for windows
    Hopefully other OS's just work tm.

--- src/extension.ts
+++ src/extension.ts
@@ -3,7 +3,6 @@ import setPlugin from "./setPlugin";
 import setLibrary from "./setLibrary";
 import setNativeLibrary from "./setNativeLibrary";
 import * as path from "path";
-import { platform } from "os";
 
 export const id = "overextended.cfxlua-vscode";
 export const extension = extensions.getExtension(id)!;
@@ -17,14 +16,13 @@ export async function activate(context: ExtensionContext) {
 	const sourceUri = Uri.joinPath(extension.extensionUri, "plugin");
 	const targetUri = Uri.joinPath(storageUri, "cfxlua");
 	storagePath = targetUri.toString();
-	// istg they go out of their way to make referencing the global storage path annoying
-	storagePath = path.join(
-		"~",
-		platform() === "win32" ? "AppData\\Roaming" : ".config",
-		storagePath.substring(storagePath.indexOf("Code")),
-	);
 
-	console.log("storagePath", storagePath);
+	if (storagePath.includes("AppData")) {
+		storagePath = path.join(
+			"~",
+			storagePath.substring(storagePath.indexOf("AppData")),
+		);
+	}
 
 	try {
 		await workspace.fs.stat(targetUri);
